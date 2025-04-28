24315edeeddb47e402b17c48152a588a9047d1da
cycneuramus <56681631+cycneuramus@users.noreply.github.com>
2025-04-28 11:10:35 +0200

    docs: Clarify registry module internals

--- src/nmgr/registry.nim
+++ src/nmgr/registry.nim
@@ -4,17 +4,19 @@ import std/[strformat, tables]
 
 type Registry*[T] = OrderedTable[string, T]
 
-proc initRegistry*[T](t: typedesc[T]): Registry[T] =
-  ## Initializes a new registry for type T
+func initRegistry*[T](t: typedesc[T]): Registry[T] =
+  ## Initializes a new registry for type T. The unused 'typedesc' parameter
+  ## is purely for the benefit of the compiler and lets one do something like
+  ## 'Cat.initRegistry' instead of, say, initRegistry[Cat]().
   initOrderedTable[string, T]()
 
-proc add*[T](registry: var Registry[T], key: string, value: T) =
+func add*[T](registry: var Registry[T], key: string, value: T) =
   ## Adds an entry to the registry
   if key in registry:
     raise newException(ValueError, fmt"'{key}' is already registered")
   registry[key] = value
 
-proc get*[T](registry: Registry[T], key: string): T =
+func get*[T](registry: Registry[T], key: string): T =
   ## Gets an entry from the registry by key
   if key in registry:
     return registry[key]
@@ -22,15 +24,16 @@ proc get*[T](registry: Registry[T], key: string): T =
 
 runnableExamples:
   type Cat = object
-    name: string
     age: int
 
-  let kitten = Cat(name: "Simba", age: 1)
-  let cat = Cat(name: "Mufasa", age: 5)
+  let kitten = Cat(age: 1)
+  let cat = Cat(age: 5)
 
   var catRegistry = Cat.initRegistry
-  catRegistry.add(kitten.name, kitten)
-  catRegistry.add(cat.name, cat)
+  catRegistry.add("Simba", kitten)
+  catRegistry.add("Mufasa", cat)
 
   assert catRegistry.get("Simba") == kitten
+  assert kitten.age == 1
   assert catRegistry.get("Mufasa") == cat
+  assert kitten.age == 5
